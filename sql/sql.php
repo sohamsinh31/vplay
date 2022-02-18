90612e84bdd7a3c0611f7fb5a462072eacc04c51
sohamsinh31 <sohamb2019@gmail.com>
2022-02-18 23:13:35 +0530

    2.0

--- sql/sql.php
+++ sql/sql.php
@@ -10,6 +10,24 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
     die("Sorry we failed to connect: ". mysqli_connect_error());
 }
-$sql = "CREATE TABLE `users` "
-$sql2 = 
+$sql =" CREATE TABLE users (
+    id varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    image varchar(255) NOT NULL,
+    PRIMARY KEY (Personid)
+    "
+);
+$sql2 = " CREATE TABLE persons (
+    id varchar(255) NOT NULL,
+    Name varchar(255) NOT NULL,
+    Description varchar(255) NOT NULL,
+    thumbpath varchar(255) NOT NULL,
+    vidpath varchar(255) NOT NULL,
+    uploadedby varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+    "
+);
+"
 ?>
